ac7f2900e9f69a2b163f8aa8f4002c0f93c4556a
Roman <w.romankornev@gmail.com>
2017-10-28 04:57:33 +0300

    [ADD] Xception architecture
    -- Bottlenecks
    -- Submission12

---
+++ xception_example.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+'''Xception V1 model for Keras.
+On ImageNet, this model gets to a top-1 validation accuracy of 0.790.
+and a top-5 validation accuracy of 0.945.
+Do note that the input image format for this model is different than for
+the VGG16 and ResNet models (299x299 instead of 224x224),
+and that the input preprocessing function
+is also different (same as Inception V3).
+Also do note that this model is only available for the TensorFlow backend,
+due to its reliance on `SeparableConvolution` layers.
+# Reference:
+- [Xception: Deep Learning with Depthwise Separable Convolutions](https://arxiv.org/abs/1610.02357)
+'''
+from __future__ import print_function
+from __future__ import absolute_import
+
+import warnings
+import numpy as np
+
+from keras.preprocessing import image
+
+from keras.models import Model
+from keras import layers
+from keras.layers import Dense
+from keras.layers import Input
+from keras.layers import BatchNormalization
+from keras.layers import Activation
+from keras.layers import Conv2D
+from keras.layers import SeparableConv2D
+from keras.layers import MaxPooling2D
+from keras.layers import GlobalAveragePooling2D
+from keras.layers import GlobalMaxPooling2D
+from keras.engine.topology import get_source_inputs
+from keras.utils.data_utils import get_file
+from keras import backend as K
+from keras.applications.imagenet_utils import decode_predictions
+from keras.applications.imagenet_utils import _obtain_input_shape
+from keras.applications import Xception
+from keras.applications.xception import preprocess_input
+
+TF_WEIGHTS_PATH = 'https://github.com/fchollet/deep-learning-models/releases/download/v0.4/xception_weights_tf_dim_ordering_tf_kernels.h5'
+TF_WEIGHTS_PATH_NO_TOP = 'https://github.com/fchollet/deep-learning-models/releases/download/v0.4/xception_weights_tf_dim_ordering_tf_kernels_notop.h5'
+
+
+if __name__ == '__main__':
+    model = Xception(include_top=True, weights='imagenet')
+
+    img_path = 'data\\dogscats\\train\\cats\\cat.10013.jpg'
+    img = image.load_img(img_path, target_size=(299, 299))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    print('Input image shape:', x.shape)
+
+    preds = model.predict(x)
+    print(np.argmax(preds))
+    print('Predicted:', decode_predictions(preds, 1))
